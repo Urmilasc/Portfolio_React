11ab3294f886d7b50e189b342e2acaaaf9fbcf17
Erik <128381361+Erik-Truckner-Teacher@users.noreply.github.com>
2023-04-07 06:35:44 -0400

    Lesson 12 Corrected

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -10,7 +10,6 @@ import Projects from './Projects'
 import WorkExperience from './WorkExperience'
 import Contact from './Contact'
 import Earth from './canvas/Earth'
-import { Perf } from 'r3f-perf'
 
 const MainContainer = () => {
   // Canvas BG color
@@ -24,7 +23,6 @@ const MainContainer = () => {
         style={{ position: 'fixed' }}
         camera={{ position: [20, 3, 5], fov: 25 }}
         onCreated={bgColor}>
-        <Perf position='bottom-left' />
         <pointLight intensity={2} color={0x61dbfb} position={[0, 5, 5]} />
         <spotLight intensity={1} color={0x61dbfb} position={(-20, 50, 10)} />
 
